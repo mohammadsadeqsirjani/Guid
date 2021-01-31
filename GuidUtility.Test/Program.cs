9b2bb53bbf8f787d758a667922285d1402cc5ac7
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-31 15:16:18 +0330

    InitialCommit

---
+++ GuidUtility.Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+
+namespace GuidUtility.Test
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sequence = new List<System.Guid>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var guid = Guid.SequentialGuid();
+                sequence.Add(guid);
+
+                Console.WriteLine(guid);
+            }
+
+            Console.WriteLine();
+
+            sequence = sequence.OrderBy(x=>x).ToList();
+
+            foreach (var guid in sequence)
+            {
+                Console.WriteLine(guid);
+            }
+        }
+    }
+}
---
+++ GuidUtility/Guid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace GuidUtility
+{
+    public struct Guid
+    {
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private class NativeMethod
+        {
+            [DllImport("rpcrt4.dll", SetLastError = true)]
+            public static extern int UuidCreateSequential(out System.Guid guid);
+        }
+
+        public static System.Guid SequentialGuid()
+        {
+            const int rpcSOk = 0;
+
+            var result = NativeMethod.UuidCreateSequential(out var guid);
+
+            return result == rpcSOk ? guid : System.Guid.NewGuid();
+        }
+
+        public static List<System.Guid> ToGuid(IEnumerable<string> @this)
+        {
+            var result = new List<System.Guid>();
+            if (@this == null) return result;
+            foreach (var item in @this)
+            {
+                if (System.Guid.TryParse(item, out var parsedGuid))
+                {
+                    result.Add(parsedGuid);
+                }
+            }
+
+            return result;
+        }
+
+        public static System.Guid ToGuid(string @this)
+        {
+            return System.Guid.TryParse(@this, out var guid) ? guid : System.Guid.Empty;
+        }
+    }
+}
